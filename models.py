f4eefa52527b4481c52a487c0b808c6dc596dfad
Aeiman191 <aimteiyaz191@gmail.com>
2024-03-30 11:17:00 +0500

    updated code in models.py and test.py

--- models.py
+++ models.py
@@ -1,10 +1,16 @@
+"""
+This module trains and evaluates several machine learning models on a dataset
+for heart disease prediction. It includes training for Decision Tree, SVM,
+and Logistic Regression classifiers, and serializes the trained models.
+"""
+
 import pandas as pd
+import pickle
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-import pickle
 
 # Load the dataset
 df = pd.read_csv('heart_v2.csv')
